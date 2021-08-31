79fc7dd83244d47a107f5695cea7948a40f3453d
tpw <1662591123@qq.com>
2021-08-31 13:04:03 +0800

    hot-fix commit1

--- src/main/java/MyTest.java
+++ src/main/java/MyTest.java
@@ -3,5 +3,6 @@ public class MyTest {
         System.out.println("hahha");
         System.out.println("你好");
         System.out.println("世界");
+        System.out.println("hello world");
     }
 }
